48a73218d92b40e26b23c9fd4a9ccdf3a4175740
23470 <23470@DESKTOP-BBHJUJ5>
2022-11-23 23:41:10 +0100

    innitial commit

---
+++ HiddenVillaServer/Data/Repository/HotelAmenityRepository.cs
@@ -0,0 +1,97 @@
+﻿using System.Collections;
+using HiddenVillaServer.Data.Repository.IRepository;
+using HiddenVillaServer.Model;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+
+
+namespace HiddenVillaServer.Data.Repository
+{
+    public class HotelAmenityRepository : IHotelAmenity
+    {
+        private readonly VillaDbContext _db;
+
+        public HotelAmenityRepository(VillaDbContext db)
+        {
+            _db = db;
+        }
+
+
+        public async Task<HotelAmenity> CreateHotelAmenity(HotelAmenity hotelAmenity)
+        {
+            var addedHotelAmenity = await _db.HotelAmmenities.AddAsync(hotelAmenity);
+            await _db.SaveChangesAsync();
+            return addedHotelAmenity.Entity;
+        }
+
+        public async Task<HotelAmenity> UpdateHotelAmenity( HotelAmenity hotelAmenity)
+        {
+            if (hotelAmenity != null)
+            {
+                var updatedamenity = _db.HotelAmmenities.Update(hotelAmenity);
+                await _db.SaveChangesAsync();
+                return updatedamenity.Entity;
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        public async Task<int> DeleteHotelAmenity(int amenityId)
+        {
+            var amenability = await _db.HotelAmmenities.FindAsync(amenityId);
+            if (amenability != null)
+            {
+                var amenityDelete = _db.HotelAmmenities.Remove(amenability);
+                return await _db.SaveChangesAsync();
+            }
+            else
+            {
+                return 0;
+            }
+
+        }
+
+        public async Task<HotelAmenity> GetHotelAmenity(int amenityId)
+        {
+            try
+            {
+                HotelAmenity chosenAmenity = await _db.HotelAmmenities.FindAsync(amenityId);
+                return chosenAmenity;
+            }
+            catch (Exception ex)
+            {
+                return null;
+            }
+
+        }
+
+        public async Task<HotelAmenity> IsSameNameAmenityAlreadyExists(string name)
+        {
+            try
+            {
+                var amenityDetails =
+                    await _db.HotelAmmenities.FirstOrDefaultAsync(x => x.Name.ToLower().Trim() == name.ToLower().Trim()
+                    );
+                return amenityDetails;
+            }
+            catch (Exception ex)
+            {
+
+            }
+            return new HotelAmenity();
+        }
+
+        public async Task<IEnumerable<HotelAmenity>> GetAllHotelAmenity()
+        {
+            return await _db.HotelAmmenities.ToListAsync();
+        }
+
+
+    }
+}
+
+
+
+    
---
+++ HiddenVilla_Web_Api/Program.cs
@@ -0,0 +1,126 @@
+using System.Configuration;
+using System.Text;
+using HiddenVilla_Web_Api.Helper;
+using HiddenVillaServer.Data;
+using HiddenVillaServer.Data.Repository.IRepository;
+using HiddenVillaServer.Data.Repository;
+using HiddenVillaServer.Service;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+using Newtonsoft.Json.Serialization;
+
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers().AddJsonOptions(opt => opt.JsonSerializerOptions.PropertyNamingPolicy = null)
+    .AddNewtonsoftJson(opt =>
+    {
+        opt.SerializerSettings.ContractResolver = new DefaultContractResolver();
+        opt.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore;
+
+    })
+    ;
+
+
+builder.Services.AddDbContext<VillaDbContext>(options =>
+                        options.UseSqlServer(builder.Configuration
+                        .GetConnectionString("DefaultConnection"))
+                        );
+builder.Services.AddIdentity<ApplicationUser,IdentityRole>(options => options.SignIn.RequireConfirmedAccount = true).AddEntityFrameworkStores<VillaDbContext>();
+// builder.Services.AddDefaultIdentity<ApplicationUser>
+//         (options => options.SignIn.RequireConfirmedAccount = true)
+//     .AddRoles<IdentityRole>()
+//     .AddEntityFrameworkStores<VillaDbContext>();
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+builder.Services.AddScoped<IHotelRoomRepo, HotelRoomRepo>();
+builder.Services.AddScoped<IHotelImagesRepository, HotelImagesRepository>();
+builder.Services.AddScoped<IHotelAmenity, HotelAmenityRepository>();
+
+
+
+builder.Services.AddRouting(options=>options.LowercaseUrls=true);
+
+var appSettingSection = builder.Configuration.GetSection("APISettings");
+builder.Services.Configure<APISettings>(appSettingSection);
+
+var apiSettings = appSettingSection.Get<APISettings>();
+var key = Encoding.ASCII.GetBytes(apiSettings.SecretKey);
+builder.Services.AddAuthentication(opt =>
+    {
+        opt.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+        opt.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+        opt.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+    }
+).AddJwtBearer(x =>
+{
+    x.RequireHttpsMetadata = false;
+    x.SaveToken = true;
+    x.TokenValidationParameters = new TokenValidationParameters()
+    {
+        ValidateIssuerSigningKey = true,
+        IssuerSigningKey = new SymmetricSecurityKey(key),
+        ValidateAudience = true,
+        ValidateIssuer = true,
+        ValidAudience = apiSettings.ValidAudience,
+        ValidIssuer = apiSettings.ValidIssuer,
+        ClockSkew = TimeSpan.Zero
+
+    };
+});
+ 
+
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        In = ParameterLocation.Header,
+        Description = "Please Bearer and then token in the field",
+        Name = "Authorization",
+        Type = SecuritySchemeType.ApiKey
+    });
+    c.AddSecurityRequirement(new OpenApiSecurityRequirement {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = "Bearer"
+                }
+            },
+            new string[] { }
+        }
+    });
+});
+
+builder.Services.AddCors(
+    x => x.AddPolicy("HiddenVilla", builder =>
+    {
+        builder.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod();
+    })
+);
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+app.UseCors("HiddenVilla");
+app.UseRouting();
+app.UseAuthentication();
+app.UseAuthorization();
+app.MapControllers();
+
+app.Run();
