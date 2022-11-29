426a105bedc5bcdac3495fd312746f5421847e7a
Valentine <chiagozieo03@gmail.com>
2022-11-29 01:19:38 +0000

    project reference issue

--- HiddenVilla_Client/Program.cs
+++ HiddenVilla_Client/Program.cs
@@ -1,11 +1,20 @@
+using Blazored.LocalStorage;
 using HiddenVilla_Client;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Sotsera.Blazor.Toaster.Core.Models;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
+builder.Services.AddToaster(config =>
+{
+    //example customizations
+    config.PositionClass = Defaults.Classes.Position.TopRight;
+    config.PreventDuplicates = true;
+    config.NewestOnTop = false;
+});
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-
+builder.Services.AddBlazoredLocalStorage();
 await builder.Build().RunAsync();
