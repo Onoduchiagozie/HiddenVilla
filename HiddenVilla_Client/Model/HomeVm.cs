b471b1c82e2aa066a19a8342bc4e2d300f90a841
Valentine <chiagozieo03@gmail.com>
2022-11-29 00:52:05 +0000

    date_validation only works with certain string date format

---
+++ HiddenVilla_Client/Model/HomeVm.cs
@@ -0,0 +1,8 @@
+﻿namespace HiddenVilla_Client.Model;
+
+public class HomeVm
+{
+    public DateTime StartDate { get; set; } = DateTime.Now;
+    public DateTime  EndDate { get; set; }
+    public int NoOfNights { get; set; } = 1;
+}
